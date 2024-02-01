96ed90ffe315f88d3fe75922f3f1440eeada1228
smsmith1234 <smsmith1234@GMAIL.COM>
2024-02-01 09:10:58 -0600

    Update ha_gym_02-01-24.ino

--- ha_gym_02-01-24.ino
+++ ha_gym_02-01-24.ino
@@ -81,7 +81,7 @@ void setup() {
     else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
     else if (error == OTA_END_ERROR) Serial.println("End Failed");
   });
-  /*********************************************************************************/
+/*********************************************************************************/
 void loop() {
   ArduinoOTA.handle();
   if (!client.connected()) {
